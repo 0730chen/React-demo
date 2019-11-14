6b69f26c2accb0e34c48c56c1fc1fad4f656052d
0730chen <1252331215@qq.com>
2019-11-14 18:20:54 +0800

    实现了知乎热榜

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "../style/Question.scss";
+import axios from "axios";
 
 //引入需要的包
 
@@ -9,32 +10,49 @@ class Question extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      focus: [
-        { username: "haha", age: "18", title: "你遇到的有趣的事" },
-        { username: "zzzz", age: "18", title: "你遇到的开心的事" },
-        { username: "xxxx", age: "18", title: "你遇到的高兴的事" },
-        { username: "风云", age: "18", title: "风云决战" },
-        { username: "风清扬", age: "18", title: "独孤九剑" }
-      ],
+      focus: [],
       bigV: [
-        { username: "牛客网" },
-        { username: "路人甲" },
-        { username: "九章算法" },
-        { username: "有雨西" },
-        { username: "兔子" },
-        { username: "阿狗" },
-        { username: "天王盖地虎" },
-		{username:'哈哈哈'},
-		{username:'王者荣耀'}
-      ]
+        { username: "知乎" },
+        { username: "微博" },
+        { username: "Github" },
+        { username: "编程语言" },
+        { username: "搜索指数" },
+        { username: "百度" },
+        { username: "前沿技术" },
+        { username: "谷歌" },
+        { username: "王者荣耀" }
+      ],
+      HotArray: [],
+      api: {}
     };
+  }
+  async getHotData() {
+    let res = await axios.get("api/hot");
+    let data = res.data;
+    this.setState({
+      HotArray: data
+    });
+  }
+  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+  handleClick(e) {
+    let tab = e.target.innerText;
+    console.log(tab);
+    // this.getHotData();
+  }
+  componentWillMount() {
+    // this.getHotData();
   }
   render() {
     return (
       <div>
         <div>显示最新的热点</div>
         <div className="question-main">
-          <div className="bigVlist">
+          <div
+            className="bigVlist"
+            onClick={e => {
+              this.handleClick(e);
+            }}
+          >
             {this.state.bigV.map((item, i) => {
               return (
                 <div className="user" key={i}>
@@ -44,11 +62,17 @@ class Question extends React.Component {
             })}
           </div>
           <ul>
-            {this.state.focus.map((item, i) => {
+            {this.state.HotArray.map((item, i) => {
               return (
-                <div key={i}>
-                  <li>{item.username}</li>
-                  <li>{item.title}</li>
+                <div key={i} className="Hot-item">
+                  <svg className="icon star" aria-hidden="true">
+                    <use xlinkHref="#icon-zu"></use>
+                  </svg>
+                  <div className="Index">{item.index}</div>
+                  <div className="Title-Container">
+                    <a href={item.Link}>{item.Title}</a>
+                    <div className="Rank">{item.Rank}</div>
+                  </div>
                 </div>
               );
             })}
